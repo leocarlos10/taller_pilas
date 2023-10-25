c91241abbf5436edc11138939ecd9aa57711ce06
leocarlos10 <leocarlosospinacausil10@gmail.com>
2023-10-24 20:49:31 -0500

    subiendo el taller

---
+++ Taller_Pilas/src/taller_pilas/nodo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller_pilas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class nodo {
+    
+    int valor;
+    
+    // apuntador
+    nodo sig;
+    
+    public nodo(int valor){
+        
+        this.valor=valor;
+    }
+}
